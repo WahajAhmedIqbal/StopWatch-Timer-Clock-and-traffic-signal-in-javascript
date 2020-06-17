53e15bd3d61beb09751ebfc12f8cad27e6e8dba5
wahaj ahmed iqbal <wahaj465@gmail.com>
2020-06-17 22:40:47 +0500

    stopwatch

---
+++ home.js
@@ -0,0 +1,33 @@
+// var interval;
+// function startimer(){
+//     interval = setInterval(timer, 1000);
+// }
+// function timer(){
+//     var date = new Date().toLocaleTimeString();
+//     document.getElementById('demo').innerHTML = date;
+// }
+// function stoptimer(){
+//     clearInterval(interval)
+// }
+// /////////////////////////////////// timer ////////////
+// var counter = 0;
+// var timelift = parseInt(prompt("Enter num"));
+// function convertsec(s)
+//     {
+//         var min = Math.floor(s / 60);
+//         var sec = s % 60;
+//         return min +  ":" + sec;
+//     }
+// var intervel = setInterval(timeIt , 1000);
+// function timeIt()
+//     {
+//         counter++;
+//         document.getElementById('timer').innerHTML = convertsec((timelift - counter));
+//         if(counter == timelift)
+//         {
+//             clearInterval(intervel);
+//             document.getElementById('paras').innerHTML = "time over";
+//         }
+
+//     }
+ 
